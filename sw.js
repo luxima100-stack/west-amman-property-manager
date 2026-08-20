e92e0d350b16dc3aabad34983f1d4d4048e658ac
luxima100-stack <luxima100@gmail.com>
2026-08-21 01:54:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='west-amman-v5-39-1';
+const CACHE='west-amman-v5-18';
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>{
